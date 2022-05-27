a69220aa3672eeb894fe4db9923446a651932d5b
pkshetty <puneethkshetty6720@gmail.com>
2022-05-27 16:32:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/signuppage/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.signuppage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] country = { "India", "USA", "China", "Japan", "Other"};
+
+    private EditText nameEditText;
+    Button button;
+    RadioButton genderradioButton;
+    RadioGroup radioGroup;
+    DatePicker simpleDatePicker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nameEditText = findViewById(R.id.etname);
+        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+        simpleDatePicker = (DatePicker) findViewById(R.id.simpleDatePicker);
+        button = (Button) findViewById(R.id.button2);
+        // perform click event on submit button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the values for day of month , month and year from a date picker
+                String day = "Day = " + simpleDatePicker.getDayOfMonth();
+                String month = "Month = " + (simpleDatePicker.getMonth() + 1);
+                String year = "Year = " + simpleDatePicker.getYear();
+                // display the values by using a toast
+                Toast.makeText(getApplicationContext(), day + "\n" + month + "\n" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+        }
+
+    public void onclickbuttonMethod(View v){
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        genderradioButton = (RadioButton)
+                findViewById(selectedId);
+        if(selectedId==-1){
+            Toast.makeText(MainActivity.this,"Nothing selected", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(MainActivity.this,genderradioButton.getText(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
